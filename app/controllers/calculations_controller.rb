cc666059b92d88a8742f351dfdcfc0e77645fe38
imeshcheryakova <imeshcher@chicagobooth.edu>
2015-07-11 22:02:20 -0500

    calc_complete

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -3,7 +3,10 @@ class CalculationsController < ApplicationController
   def word_count
     @text = params[:user_text]
     @special_word = params[:user_word]
-
+    @text_capitalize_all = []
+    @text.split.each do |word|
+      @text_capitalize_all.push(word.capitalize)
+    end
     # ================================================================================
     # Your code goes below.
     # The text the user input is in the string @text.
@@ -11,19 +14,30 @@ class CalculationsController < ApplicationController
     # ================================================================================
 
 
-    @character_count_with_spaces = "Replace this string with your answer."
+    @character_count_with_spaces = @text.length
 
-    @character_count_without_spaces = "Replace this string with your answer."
+    @character_count_without_spaces = @text.gsub(" ","").length
 
-    @word_count = "Replace this string with your answer."
+    @word_count = @text.split.count
 
-    @occurrences = "Replace this string with your answer."
+    @occurrences = @text_capitalize_all.count(@special_word.capitalize)
   end
 
   def loan_payment
     @apr = params[:annual_percentage_rate].to_f
     @years = params[:number_of_years].to_i
     @principal = params[:principal_value].to_f
+    if @apr>1
+      @monthly_rate = @apr/12/100
+    else
+      @monthly_rate = @apr/12
+      @apr = @apr*100
+    end
+
+    @denominator=((@monthly_rate+1)**(@years*12))-1
+    @fraction=@monthly_rate/@denominator
+    @rateandfraction=@fraction+@monthly_rate
+
 
     # ================================================================================
     # Your code goes below.
@@ -32,7 +46,8 @@ class CalculationsController < ApplicationController
     # The principal value the user input is in the decimal @principal.
     # ================================================================================
 
-    @monthly_payment = "Replace this string with your answer."
+    @monthly_payment = @rateandfraction*@principal
+
   end
 
   def time_between
@@ -48,42 +63,87 @@ class CalculationsController < ApplicationController
     #   number of seconds as a result.
     # ================================================================================
 
-    @seconds = "Replace this string with your answer."
-    @minutes = "Replace this string with your answer."
-    @hours = "Replace this string with your answer."
-    @days = "Replace this string with your answer."
-    @weeks = "Replace this string with your answer."
-    @years = "Replace this string with your answer."
+    @seconds = @ending-@starting
+    @minutes = @seconds/60
+    @hours = @minutes/60
+    @days = @hours/24
+    @weeks = @days/7
+    @years = @days/365
   end
 
   def descriptive_statistics
     @numbers = params[:list_of_numbers].gsub(',', '').split.map(&:to_f)
+
 
     # ================================================================================
     # Your code goes below.
     # The numbers the user input are in the array @numbers.
     # ================================================================================
 
-    @sorted_numbers = "Replace this string with your answer."
+    @sorted_numbers = @numbers.sort
 
-    @count = "Replace this string with your answer."
+    @count = @numbers.count
 
-    @minimum = "Replace this string with your answer."
+    @minimum = @numbers.min
 
-    @maximum = "Replace this string with your answer."
+    @maximum = @numbers.max
 
-    @range = "Replace this string with your answer."
+    @range = @maximum-@minimum
 
-    @median = "Replace this string with your answer."
+    #Median calculation for even array
 
-    @sum = "Replace this string with your answer."
+    @count_middle=@count/2
+    @position1=@count_middle-0.5
+    @position2=@count_middle+0.5
+    @number1=@sorted_numbers[@position1]
+    @number2=@sorted_numbers[@position2]
 
-    @mean = "Replace this string with your answer."
+    #Median calculation for odd array
+    @count_middle_even=@count/2
 
-    @variance = "Replace this string with your answer."
+    if @count%2 == 0
+      @median = (@number1+@number2)/2
+    else
+      @median=@sorted_numbers[@count_middle_even]
+    end
 
-    @standard_deviation = "Replace this string with your answer."
+# Sum calculation without using .sum
+    @sum=0
+    @numbers.each do |integer|
+      @sum=@sum+integer
+    end
 
-    @mode = "Replace this string with your answer."
+    @mean = @sum/@count
+
+# Variance calculation
+
+    @sum_distances=0
+    @numbers.each do |element|
+      @sum_distances=(@sum_distances+(element-@mean)**2)
+
+    end
+
+    @variance = @sum_distances/@count
+
+    @standard_deviation = Math.sqrt(@variance)
+
+    #Mode calculation
+
+    number_with_largest_count = 0
+    largest_count_so_far = 0
+
+    @numbers.each do |number|
+      if @numbers.count(number)>largest_count_so_far
+        number_with_largest_count = number
+        largest_count_so_far = @numbers.count(number)
+      end
+    end
+
+    if largest_count_so_far==1
+     @mode ="no mode"
+    else
+      @mode = number_with_largest_count
+    end
+
   end
 end
